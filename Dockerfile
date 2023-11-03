27aeea7bc580e8809b55238a883bc560a5ed4f84
LIGHTJ60 <jligh43@wgu.edu>
2023-11-03 16:16:25 -0600

    Final Edits

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM openjdk:21-slim
 LABEL authors="LIGHTJ60"
 
-#comment
 WORKDIR /app
 
 COPY ./target/D387_sample_code-0.0.2-SNAPSHOT.jar /app/app.jar
--- src/main/java/edu/wgu/d387_sample_code/model/request/WelcomeMessage.java
+++ src/main/java/edu/wgu/d387_sample_code/model/request/WelcomeMessage.java
@@ -7,42 +7,30 @@ import java.util.ResourceBundle;
 
 public class WelcomeMessage {
     List<String> list = new ArrayList<>();
-
     public WelcomeMessage() {
         Thread thread1 = new WelcomeThread1();
         Thread thread2 = new WelcomeThread2();
         thread1.start();
         thread2.start();
-
-        try {
-            thread1.join(); // Waits for thread1 to die
-            thread2.join(); // Waits for thread2 to die
-        } catch (InterruptedException e) {
-            // Handle the interruption appropriately
-            Thread.currentThread().interrupt();
-            System.out.println("Thread execution was interrupted.");
-        }
     }
 
-    public class WelcomeThread1 extends Thread {
-        public void run() {
+    public class WelcomeThread1 extends Thread{
+        public void run(){
             String english = ResourceBundle.getBundle("locals", Locale.ENGLISH).getString("greeting");
-            synchronized (list) {
-                list.add(english);
-            }
+            list.add(english);
         }
     }
-
-    public class WelcomeThread2 extends Thread {
-        public void run() {
+    public class WelcomeThread2 extends Thread{
+        public void run(){
             String french = ResourceBundle.getBundle("locals", Locale.CANADA_FRENCH).getString("greeting");
-            synchronized (list) {
-                list.add(french);
-            }
+            list.add(french);
         }
     }
 
-    public synchronized List<String> getWelcomeMessage() {
-        return new ArrayList<>(list); // Return a copy to avoid modification from outside
+
+    public List<String> getWelcomeMessage(){
+        return list;
     }
 }
+
+
